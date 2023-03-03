4c5e8cee08b8ecd7db060d55fda62ec5496bf6d1
unknown <rifatkhabibulin88@gmail.comgit config --global user.name Rifatgit statusgit config user.email rifatkhabibulin88@gmail.com>
2023-03-03 10:13:29 +0300

    feat: add header and hero comp

---
+++ src/components/Hero.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="hero">
+        <div class="hero-info">
+            <div class="hero-info-text">
+                <h2 class="hero-info__title">
+                Luxury homeware for people who love timeless design quality
+                </h2>
+                <span class="hero-info__description">
+                    Shop the new Spring 2022 collection today
+                </span>
+            </div>
+            <a href="#" class="hero-info__link">View collection</a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+        background: url('/img/hero.jpg');
+        height: 704px;
+        background-size: cover;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 80px;
+        &-info {
+            background-color: #fff;
+            max-width: 630px;
+            padding: 50px 55px;
+            height: 444px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            top: 130px;
+            right: 80px;
+            &__link {
+                color: #2A254B;
+                text-decoration: none;
+                &:hover {
+                    text-decoration: underline;
+                }
+            }
+            &__title {
+                font-family: 'Clash Display', sans-serif;
+                font-weight: 400;
+                font-size: 32px;
+                margin: 0 0 20px 0;
+                line-height: 1.5;
+            }
+            &__description {
+                color: #5B5676;
+                font-size: 18px;
+            }
+        }
+    }
+</style>
+
+
+
---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,97 @@
+<template>
+    <header class="header">
+        <div class="header-top">
+            <div class="header-top-search">
+                <img src="/svg/header-search.svg" alt="search">
+            </div>
+            <a href="#" class="header-logo">Avion</a>
+            <div class="header-top-right">
+                <div class="header-top-right__cart">
+                    <a href="#">
+                        <img src="/svg/header-cart.svg" alt="cart">
+                    </a>
+                </div>
+                <div class="header-top-right__user">
+                    <a href="#">
+                        <img src="/svg/header-user.svg" alt="user">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="header-menu">
+            <a 
+            class="header-menu__link"
+            :href="element.path" 
+            v-for="(element, i) of menu"
+            :key="i"
+            >
+                {{ element.name }}
+            </a>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const menu = [
+    {
+        name: 'Plants pots',
+        path: '/plant'
+    },
+    {
+        name: 'Ceramics',
+        path: '/ceramics'
+    },
+    {
+        name: 'Tables',
+        path: '/tables'
+    },
+]
+</script>
+
+<style lang="scss" scoped>
+.header {
+    height: 132px;
+    background: #fff;
+    &-top {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        align-items: center;
+        height: 70px;
+        border-bottom: 1px solid rgba(0, 0, 0, .1);
+        &-right {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        &__cart {
+            margin-right: 16px;
+        }
+    }
+    }
+    &-logo {
+        color: #22202E;
+        font-size: 24px;
+        text-decoration: none;
+        font-family: 'Clash Display', sans-serif;
+        text-align: center;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+    &-menu {
+        height: 62px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        &__link {
+            margin: 0 22px;
+            color: #726E8D;
+            text-decoration: none;
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+    }
+}
+</style>
